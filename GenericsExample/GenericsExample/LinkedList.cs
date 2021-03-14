d63b99f8be8546b8e998cc00b48390088e381eb6
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2021-03-14 17:04:31 +0200

    Generic example
    Generic example

---
+++ GenericsExample/GenericsExample/LinkedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenericsExample
+{
+    class LinkedList<T>
+    {
+        public LinkedList<T> NextElement { get; set; }
+        public T Value { get; set; }
+
+        public LinkedList(T val)
+        {
+            NextElement = null;
+            Value = val;
+        }
+
+        public void PrintLinkedList()
+        {
+            var curElement = this;
+
+            while (curElement != null)
+            {
+                Console.WriteLine($"Element {curElement.Value}");
+                curElement = curElement.NextElement;
+            }
+        }
+    }
+}
---
+++ GenericsExample/GenericsExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GenericsExample
+{
+    class Program
+    {
+        public LinkedList<int> IntLinkedList;
+        public LinkedList<string> StringLinkedList;
+        static void Main()
+        {
+            var prog = new Program();
+            Console.WriteLine("Integer linked list:");
+            prog.IntLinkedList.PrintLinkedList();
+            Console.WriteLine("String linked list:");
+            prog.StringLinkedList.PrintLinkedList();
+        }
+
+        public Program()
+        {
+            IntLinkedList = new LinkedList<int>(1);
+            InitIntLinkedList();
+
+            StringLinkedList = new LinkedList<string>("a");
+            InitStringLinkedList();
+        }
+
+        void InitIntLinkedList()
+        {
+            var curElement = IntLinkedList;
+            for (int i = 2; i < 10; i++)
+            {
+                curElement.NextElement = new LinkedList<int>(i);
+                curElement = curElement.NextElement;
+            }
+        }
+
+        void InitStringLinkedList()
+        {
+            var curElement = StringLinkedList;
+            var chr = curElement.Value.ToCharArray()[0];
+            for (int i = 1; i < 10; i++)
+            {
+                curElement.NextElement = new LinkedList<string>($"{curElement.Value}{++chr}");
+                curElement = curElement.NextElement;
+            }
+        }
+    }
+}
